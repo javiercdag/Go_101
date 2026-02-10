93096b33256386e6aafe43ceba0a28c1f9be9603
javiercdag <javiercdag@gmail.com>
2026-02-10 16:18:58 +0100

    solved 04-01

--- 04-type-system/01-construct-duration/exercise.go
+++ 04-type-system/01-construct-duration/exercise.go
@@ -6,5 +6,5 @@ import "time"
 
 // constructTime constructs a `Time` instant based on its two arguments (arg1, arg2)
 func constructDuration(arg1 int, arg2 int) time.Duration {
-	// INSERT YOUR CODE HERE
+	return time.Duration(arg1)*time.Hour + time.Duration(arg2)*time.Minute
 }
