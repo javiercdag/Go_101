1d2fb43ecc9074c9bc8babbb3247e2e43f7c01d4
javiercdag <javiercdag@gmail.com>
2026-02-10 09:56:26 +0100

    solved 02-01

--- 02-types/01-booleans/exercise.go
+++ 02-types/01-booleans/exercise.go
@@ -3,4 +3,14 @@ package logicalops
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func inverse(b bool) bool {
+	return !b
+}
+
+func or(x, y bool) bool {
+	return x || y
+}
+
+func deMorgan(a, b bool) bool {
+	return or(inverse(a), inverse(b))
+}
