4faed77c68083f33919370b898ef82ee890a5c08
javiercdag <javiercdag@gmail.com>
2026-02-10 10:09:09 +0100

    solved 02-02

--- 02-types/02-numbers/exercise.go
+++ 02-types/02-numbers/exercise.go
@@ -1,10 +1,29 @@
 package calculator
 
 import (
-	"fmt"
+	"math"
+	"strconv"
 )
 
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func amean(x, y float64) int {
+	return int(math.Round((x + y) / 2.0))
+}
+
+func ameanString(x, y string) (int, error) {
+	xfloat, err := strconv.ParseFloat(x, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	yfloat, err := strconv.ParseFloat(y, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return amean(xfloat, yfloat), nil
+}
