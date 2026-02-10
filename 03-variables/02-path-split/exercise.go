75ab76c39889cf0243df1bd4197ac455848d999b
javiercdag <javiercdag@gmail.com>
2026-02-10 11:33:22 +0100

    solved 03-02

--- 03-variables/02-path-split/exercise.go
+++ 03-variables/02-path-split/exercise.go
@@ -5,4 +5,9 @@ import "path"
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+// splitPath returns the directory component of a file path.
+func splitPath(fullPath string) string {
+	dir, _ := path.Split(fullPath)
+
+	return dir
+}
