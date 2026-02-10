baff1d956e1feeac26519f3f4463fb9e6777e8b7
javiercdag <javiercdag@gmail.com>
2026-02-10 16:53:52 +0100

    solved 04-02

--- 04-type-system/02-secret-protocol-header/exercise.go
+++ 04-type-system/02-secret-protocol-header/exercise.go
@@ -4,6 +4,20 @@ package secretprotocolheader
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // createPublishFixHeader constructs an octet (8-bit long byte) based on its three arguments and the fix QoS setting.
-func createPublishFixHeader(isFirstAttempt bool, isBroadcasted bool, isSecure bool) byte {
-	// INSERT YOUR CODE HERE
+func createPublishFixHeader(isFirstAttempt, isBroadcasted, isSecure bool) byte {
+	var octet byte = (1 << 6) | (1 << 2)
+
+	if isFirstAttempt {
+		octet |= 1 << 4
+	}
+
+	if isBroadcasted {
+		octet |= 1 << 1
+	}
+
+	if isSecure {
+		octet |= 1 << 0
+	}
+
+	return octet
 }
