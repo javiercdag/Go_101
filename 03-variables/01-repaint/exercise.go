da18e897fcba95953cb79937d4c9f4155c604afa
javiercdag <javiercdag@gmail.com>
2026-02-10 11:24:21 +0100

    solved 03-01

--- 03-variables/01-repaint/exercise.go
+++ 03-variables/01-repaint/exercise.go
@@ -5,7 +5,28 @@ import "fmt"
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
+var colorMap = map[string]string{
+	"red":        "green",
+	"green":      "red",
+	"blue":       "orange",
+	"orange":     "blue",
+	"yellow":     "purple",
+	"purple":     "yellow",
+	"amber":      "violet",
+	"violet":     "amber",
+	"teal":       "vermilion",
+	"vermilion":  "teal",
+	"magenta":    "chartreuse",
+	"chartreuse": "magenta",
+}
+
 // repaintColor returns the complementary of the color received as argument or an error for an unknown error.
 func repaintColor(color string) (string, error) {
-	// INSERT YOUR CODE HERE
+	val, ok := colorMap[color]
+
+	if !ok {
+		return "", fmt.Errorf("unknown color: %s", color)
+	}
+
+	return val, nil
 }
