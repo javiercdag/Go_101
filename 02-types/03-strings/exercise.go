a6786cbcca32ba4127d1ba9028e7b08613478f2a
javiercdag <javiercdag@gmail.com>
2026-02-10 10:35:21 +0100

    solved 02-03

--- 02-types/03-strings/exercise.go
+++ 02-types/03-strings/exercise.go
@@ -1,6 +1,61 @@
 package strings
 
+import (
+	"unicode/utf8"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func multilineString() string {
+	return "some\nmultiline\nstring"
+}
+
+func stringLen(s string) int {
+	return utf8.RuneCountInString(s)
+}
+
+func trimFirstChar(s string) string {
+	if stringLen(s) <= 0 {
+		return s
+	}
+
+	_, size := utf8.DecodeRuneInString(s)
+
+	return s[size:]
+}
+
+func trimLastChar(s string) string {
+	if stringLen(s) <= 0 {
+		return s
+	}
+
+	_, size := utf8.DecodeLastRuneInString(s)
+
+	return s[:len(s)-size]
+}
+
+func swapFirstChar(s string) string {
+	if stringLen(s) == 0 {
+		return s
+	}
+
+	// "A" + string is optimized by the runtime
+	return "A" + trimFirstChar(s)
+}
+
+func swapLastChar(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	return trimLastChar(s) + "A"
+}
+
+func prependChar(s string) string {
+	return "A" + s
+}
+
+func appendChar(s string) string {
+	return s + "A"
+}
