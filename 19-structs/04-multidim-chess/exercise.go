29210fc1b7b10c53df001e58866ba55fa2c6b511
javiercdag <javiercdag@gmail.com>
2026-02-11 15:55:03 +0100

    solved 19-04

--- 19-structs/04-multidim-chess/exercise.go
+++ 19-structs/04-multidim-chess/exercise.go
@@ -17,4 +17,20 @@ type Board struct {
 	Pieces []Piece
 }
 
-// INSERT YOUR CODE HERE
+func copyPosition(p Position) Position {
+	return Position{Coordinates: []int{p.Coordinates[0], p.Coordinates[1]}}
+}
+
+func copyPiece(p Piece) Piece {
+	return Piece{Type: p.Type, Color: p.Color, Position: copyPosition(p.Position)}
+}
+
+func copyBoard(b Board) Board {
+	newPieces := make([]Piece, len(b.Pieces))
+
+	for i, p := range b.Pieces {
+		newPieces[i] = copyPiece(p)
+	}
+
+	return Board{Pieces: newPieces}
+}
