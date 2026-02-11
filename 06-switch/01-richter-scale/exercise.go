2bce90530436482bee7c4a1477e87bc0eda4e091
javiercdag <javiercdag@gmail.com>
2026-02-11 08:35:40 +0100

    solved 06-01

--- 06-switch/01-richter-scale/exercise.go
+++ 06-switch/01-richter-scale/exercise.go
@@ -5,5 +5,28 @@ package richterscale
 
 // describeEarthquake returns the "description" of a given magnitude value on the Richter scale.
 func describeEarthquake(magnitude float32) string {
-	// INSERT YOUR CODE HERE
+	scale := ""
+
+	switch {
+	case magnitude < 2.0:
+		scale = "micro"
+	case magnitude < 3.0:
+		scale = "very minor"
+	case magnitude < 4.0:
+		scale = "minor"
+	case magnitude < 5.0:
+		scale = "light"
+	case magnitude < 6.0:
+		scale = "moderate"
+	case magnitude < 7.0:
+		scale = "strong"
+	case magnitude < 8.0:
+		scale = "major"
+	case magnitude < 10.0:
+		scale = "great"
+	case magnitude >= 10.0:
+		scale = "massive"
+	}
+
+	return scale
 }
