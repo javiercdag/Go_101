0bbf33bab6a3828aa938331a0e67cbf4244265c1
javiercdag <javiercdag@gmail.com>
2026-02-11 10:54:35 +0100

    solved 08-03

--- 08-arrays/03-filtering-data/exercise.go
+++ 08-arrays/03-filtering-data/exercise.go
@@ -5,5 +5,23 @@ package filteringdata
 
 // filterData filters a slice based in an index slice.
 func filterData(keys []string, indices []int) [10]string {
-	// INSERT YOUR CODE HERE
+	resultingArray := [10]string{}
+	counter := 0
+
+	if len(keys) != len(indices) {
+		return resultingArray
+	}
+
+	for i := 0; i < len(keys); i++ {
+		if counter > 9 {
+			return resultingArray
+		}
+
+		if indices[i]%2 != 0 {
+			resultingArray[counter] = keys[i]
+			counter++
+		}
+	}
+
+	return resultingArray
 }
