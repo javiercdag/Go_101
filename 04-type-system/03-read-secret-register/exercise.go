67a1fd0d35c6889a66474729e772476bb99fc170
javiercdag <javiercdag@gmail.com>
2026-02-11 08:22:24 +0100

    solved 04-03

--- 04-type-system/03-read-secret-register/exercise.go
+++ 04-type-system/03-read-secret-register/exercise.go
@@ -5,5 +5,10 @@ package readsecretregister
 
 // parseChannelControlRegister constructs 4 octets (8-bit long uint) based on the parameter register.
 func parseChannelControlRegister(charCtrl uint32) (uint8, uint8, uint8, uint8) {
-	// INSERT YOUR CODE HERE
+	RX_PCODE := uint8(charCtrl >> 24)
+	RX_CHAN := uint8(charCtrl >> 16)
+	TX_CHAN := uint8(charCtrl >> 8)
+	TX_PCODE := uint8(charCtrl) // top 24 bits are chopped off
+
+	return TX_CHAN, RX_CHAN, RX_PCODE, TX_PCODE
 }
