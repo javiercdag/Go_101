46be918ab42d33cffa5025f28013c8427bd00170
javiercdag <javiercdag@gmail.com>
2026-02-11 08:48:13 +0100

    solved 07-01

--- 07-loops/01-factorial-sum-abs/exercise.go
+++ 07-loops/01-factorial-sum-abs/exercise.go
@@ -3,4 +3,10 @@ package factorial
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func calcSum(n int) int {
+	if n == 1 {
+		return 1
+	}
+
+	return n + calcSum(n-1)
+}
