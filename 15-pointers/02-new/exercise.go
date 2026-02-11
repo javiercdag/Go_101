3380d3f6c1f67da64a0e9455d8850fa87b25a830
javiercdag <javiercdag@gmail.com>
2026-02-11 12:48:20 +0100

    solved 15-02

--- 15-pointers/02-new/exercise.go
+++ 15-pointers/02-new/exercise.go
@@ -2,3 +2,9 @@ package pointernew
 
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+func newValue() *float64 {
+	newFloat := new(float64)
+	*newFloat = 12.05
+	return newFloat
+}
