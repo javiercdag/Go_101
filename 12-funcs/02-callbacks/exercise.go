bba5b455cedf210ad22a6d9390cdaadf06942f4a
javiercdag <javiercdag@gmail.com>
2026-02-11 12:32:09 +0100

    solved 12-02

--- 12-funcs/02-callbacks/exercise.go
+++ 12-funcs/02-callbacks/exercise.go
@@ -2,3 +2,37 @@ package calculator
 
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+type OperationType int
+
+const (
+	Add OperationType = iota
+	Subtract
+	Multiply
+)
+
+func (op OperationType) String() string{
+	switch op {
+	case Add:
+		return "Add"
+	case Subtract:
+		return "Subtract"
+	case Multiply:
+		return "Multiply"
+	}
+
+	return "not implemented"
+}
+
+func Calculate(op OperationType, a, b float64) float64{
+	switch op {
+	case Add:
+		return a + b
+	case Subtract:
+		return a - b
+	case Multiply:
+		return a * b
+	}
+
+	return -1;
+}
