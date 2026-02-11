2451dc519e1bd242011e72dd92cd46d3d8b8466c
javiercdag <javiercdag@gmail.com>
2026-02-11 12:42:32 +0100

    solved 12-03 CLOSURES

--- 12-funcs/03-closures/exercise.go
+++ 12-funcs/03-closures/exercise.go
@@ -1,7 +1,18 @@
 package closures
 
+import "errors"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
-func proxy(...){
-	// INSERT YOUR CODE HERE
+func proxy(limit int, f func() int) func() (int, error) {
+	count := 0
+
+	return func() (int, error) {
+		if count < limit {
+			count++
+			return f(), nil
+		}
+
+		return 0, errors.New("limit exceeded")
+	}
 }
