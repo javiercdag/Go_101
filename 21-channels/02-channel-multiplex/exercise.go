ae4fd547f309b62125bb5bb34e3cbc02973721ef
javiercdag <javiercdag@gmail.com>
2026-02-13 11:58:19 +0100

    wip

--- 21-channels/02-channel-multiplex/exercise.go
+++ 21-channels/02-channel-multiplex/exercise.go
@@ -1,6 +1,42 @@
 package channelmultiplexer
 
+import (
+	"context"
+
+	"golang.org/x/sync/errgroup"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func channelMultiplex(ctx context.Context, inputs []chan any) chan any {
+	returnChan := make(chan any)
+
+	var eg errgroup.Group
+
+	for w := 0; w < len(inputs); w++ {
+		eg.Go(func() error {
+			return safeBridge(ctx, inputs[w], returnChan)
+		})
+	}
+
+	return returnChan
+}
+
+func safeBridge(ctx context.Context, in <-chan any, out chan<- any) error {
+	for {
+		select {
+		case msg := <-in:
+			select {
+			case out <- msg:
+			case <-ctx.Done():
+				close(out)
+				return nil
+			}
+
+		case <-ctx.Done():
+			close(out)
+			return nil
+		}
+	}
+}
