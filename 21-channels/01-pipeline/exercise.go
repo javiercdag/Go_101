26f165c793bc5195d88c91de6bac4dbf112218f6
javiercdag <javiercdag@gmail.com>
2026-02-13 11:07:58 +0100

    solved 21-01

--- 21-channels/01-pipeline/exercise.go
+++ 21-channels/01-pipeline/exercise.go
@@ -3,4 +3,38 @@ package pipeline
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func generator(nums []int) <-chan int {
+	results := make(chan int)
+
+	go func() {
+		for _, n := range nums {
+			results <- n
+		}
+		close(results)
+	}()
+
+	return results
+}
+
+func adder(in <-chan int) <-chan float32 {
+	results := make(chan float32)
+
+	go func() {
+		for n := range in {
+			results <- float32(n + 2)
+		}
+		close(results)
+	}()
+
+	return results
+}
+
+func collector(in <-chan float32) []float32 {
+	collected := []float32{}
+
+	for n := range in {
+		collected = append(collected, n)
+	}
+
+	return collected
+}
