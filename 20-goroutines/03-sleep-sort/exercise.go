bf4019c5e6b2432347592b7d1331b019abb2a5b9
javiercdag <javiercdag@gmail.com>
2026-02-13 10:05:34 +0100

    Solved 20-03

--- 20-goroutines/03-sleep-sort/exercise.go
+++ 20-goroutines/03-sleep-sort/exercise.go
@@ -1,6 +1,45 @@
 package sleepSort
 
+import (
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+// reverseSleepSort returns the input uint-slice sorted in the reverse order.
+func reverseSleepSort(input []uint) []uint {
+	if len(input) <= 0 {
+		return input
+	}
+
+	reverseSorted := []uint{}
+
+	results := make(chan uint, len(input))
+
+	var eg errgroup.Group
+
+	for w := 0; w < len(input); w++ {
+		eg.Go(func() error {
+			inputNumber := input[w]
+			delay := time.Duration(500-inputNumber*10) * time.Millisecond
+			time.Sleep(delay)
+			results <- inputNumber
+
+			return nil
+		})
+	}
+
+	go func() {
+		_ = eg.Wait()
+		close(results)
+	}()
+
+	for individualNumber := range results {
+		reverseSorted = append(reverseSorted, individualNumber)
+	}
+
+	return reverseSorted
+}
