9b00347ae75470d5370ae65382e8a7e65e84c478
javiercdag <javiercdag@gmail.com>
2026-02-16 10:42:28 +0100

    solved 21-03

--- 21-channels/03-channel-broadcast/exercise.go
+++ 21-channels/03-channel-broadcast/exercise.go
@@ -1,6 +1,42 @@
 package channelbroadcaster
 
+import (
+	"context"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func channelBroadcast(ctx context.Context, input <-chan any, outputs []chan<- any) {
+	go safeBridge(ctx, input, outputs)
+}
+
+func safeBridge(ctx context.Context, in <-chan any, out []chan<- any) {
+
+	defer func() {
+		for _, output := range out {
+			close(output)
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case msg, ok := <-in:
+			if !ok {
+				return
+			}
+
+			for _, output := range out {
+				select {
+				case output <- msg:
+					// res
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}
+}
